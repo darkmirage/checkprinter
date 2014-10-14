00b751ff00a5ab66e261afa7ca7bff397d9d8c70
Raven Jiang <raven@cs.stanford.edu>
2014-10-13 19:44:50 -0700

    Swapped BeautifulSoup for lxml

--- printer.py
+++ printer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import urllib2
 from datetime import datetime
-from BeautifulSoup import BeautifulSoup
+from lxml import html
 
 response = urllib2.urlopen('https://cardenalprinter.stanford.edu/hp/device/this.LCDispatcher')
-html = response.read()
-parsed_html = BeautifulSoup(html)
 
-status = parsed_html.body.find('span', attrs={'id':'Text17'}).text
+tree = html.fromstring(response.read())
+status = tree.xpath("//span[@id='Text17']/text()")
+# status = parsed_html.body.find('span', attrs={'id':'Text17'}).text
+status = status[0]
 
 if status.find('Empty') >= 0:
   raise Exception('%s: Printer tray is empty' % datetime.now())
@@ -15,3 +16,4 @@ elif status.find('OK') >= 0:
   print '%s: Printer is fine' % datetime.now()
 else:
   raise Exception('%s: Unknown printer status: %s' % (datetime.now(), status))
+
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /afs/ir.stanford.edu/users/j/c/jcx/scripts/printer
-source ./venv/bin/activate
-python printer.py
-
