cdddd7805efb3f98fea85f17fdc534988960ccd3
Raven Jiang <raven@cs.stanford.edu>
2014-10-23 00:52:34 -0700

    Handles offset by one when printer status message shows up

--- printer.py
+++ printer.py
@@ -6,14 +6,16 @@ from datetime import datetime
 response = urllib2.urlopen('https://cardenalprinter.stanford.edu/hp/device/this.LCDispatcher')
 
 html = response.read()
-m = re.findall('id=\"Text17\".*?>(.*?)</span>', html)
+m1 = re.findall('id=\"Text17\".*?>(.*?)</span>', html)
+m2 = re.findall('id=\"Text18\".*?>(.*?)</span>', html)
 
-status = m[0]
+status1 = m1[0]
+status2 = m2[0]
 
-if status.find('Empty') >= 0:
+if status1.find('Empty') >= 0 or status2.find('Empty') >= 0:
   raise Exception('%s: Printer tray is empty' % datetime.now())
-elif status.find('OK') >= 0:
+elif status1.find('OK') >= 0 or status2.find('OK'):
   print '%s: Printer is fine' % datetime.now()
 else:
-  raise Exception('%s: Unknown printer status: %s' % (datetime.now(), status))
+  raise Exception('%s: Unknown printer status: %s | %s' % (datetime.now(), status1, status2))
 
